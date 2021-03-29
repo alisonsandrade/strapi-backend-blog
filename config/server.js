05b19eb29d9615d4b0075ab89903bdc461a06096
Alison Andrade <alison.sandrade@hotmail.com>
2021-03-29 01:39:53 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '17d34bf26960cfcbdf639ed7e277540e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5481ab2e-5e3b-48c1-a891-877535d9870b'
+};
